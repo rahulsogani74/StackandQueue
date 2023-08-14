02af23aaa87af5067bccfba0872069da4b20df42
rahulsogani74 <131875485+rahulsogani74@users.noreply.github.com>
2023-08-14 09:41:22 +0530

    Add files via upload

---
+++ SlidingWindowMaximum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class SlidingWindowMaximum {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && deque.peek() < i - k + 1) {
+                deque.poll();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offer(i);
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peek()];
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        SlidingWindowMaximum solution = new SlidingWindowMaximum();
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int[] maxInWindow = solution.maxSlidingWindow(nums, k);
+        System.out.println("Maximum elements in sliding windows of size " + k + ":");
+        for (int max : maxInWindow) {
+            System.out.print(max + " ");
+        }
+    }
+}
